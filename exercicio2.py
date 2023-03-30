be5a7f7466d84ac706ce8b5675995726487c536c
unknown <nickloregiansenai@gmail.com>
2023-03-30 17:10:38 -0300

    neste repositório Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.

---
+++ exercicio2.py
@@ -0,0 +1,12 @@
+realnunlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+umaVariavel = 0 
+
+for i in range(10):
+    umaVariavel = realnunlist.pop()
+    print(umaVariavel)
+    
+print(realnunlist)
+
+
+
