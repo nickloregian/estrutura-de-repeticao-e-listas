e47395be6c4a34d15b8ff8ee0205aba90b9174f0
unknown <nickloregiansenai@gmail.com>
2023-04-06 16:19:23 -0300

    fiz um Programa que de 20 números inteiros e armazenarei num vetor. Armazenei os números pares no vetor par e os números impares no vetor impar

---
+++ exercicio5.py
@@ -0,0 +1,12 @@
+exerc = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20']
+
+print(exerc)
+
+for pares in exerc:
+    if pares not in "1,3,5,7,9,11,13,15,17,19":
+        print("pares",pares)
+
+for impares in exerc:
+    if impares not in "2,4,6,8,10,12,14,16,18,20":
+        print("impares",impares)
+
