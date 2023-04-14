0eff610e7115ff16926f9d7632a18da45750e587
unknown <nickloregiansenai@gmail.com>
2023-04-14 10:53:19 -0300

    fiz um programa permitindo ao usuário informar as populações e as taxas de crescimento iniciais

---
+++ exercicios_repetiçao/exercicio5repetição.py
@@ -0,0 +1,15 @@
+populaçãoA = float(input("a população da cidade A "))
+populaçãoB = float(input("a população da cidade B "))
+ano = 0
+
+taxa_crescimentoA = float(input("taxa de crescimento da população da cidade A "))
+taxa_crescimentoB = float(input("taxa de crescimento da população da cidade B "))
+while populaçãoA < populaçãoB:
+    
+	populaçãoA += round((populaçãoA*taxa_crescimentoA)/100)
+	populaçãoB += round((populaçãoB*taxa_crescimentoB)/100)
+	ano = ano+1
+
+print("levará",ano,"anos para população da cidade A ser maior que a cidade B")
+print("populaçãoB-->",populaçãoB,"habitantes")
+print("populaçãoA-->", populaçãoA,"habitantes")
